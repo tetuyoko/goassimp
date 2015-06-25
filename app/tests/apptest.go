a3a0f2ac2181dea8e1481dde78235b5f511f8401
tetuyoko <tyokoyama53@gmail.com>
2015-06-25 19:31:25 +0900

    formated

--- app/tests/apptest.go
+++ app/tests/apptest.go
@@ -1,36 +1,35 @@
 package tests
 
 import (
-    "github.com/revel/revel/testing"
+	"github.com/revel/revel/testing"
 )
 
 type ApplicationTest struct {
-    testing.TestSuite
+	testing.TestSuite
 }
 
 func (t *ApplicationTest) Before() {
-    println("Set up")
+	println("Set up")
 }
 
 func (t *ApplicationTest) TestThatIndexPageWorks() {
-    t.Get("/")
-    t.AssertOk()
-    t.AssertContentType("text/html; charset=utf-8")
+	t.Get("/")
+	t.AssertOk()
+	t.AssertContentType("text/html; charset=utf-8")
 }
 
 func (t *ApplicationTest) TestThatRedisPingAPIWorks() {
-    t.Get("/redis/ping")
-    t.AssertOk()
-    t.AssertContentType("application/json; charset=utf-8")
+	t.Get("/redis/ping")
+	t.AssertOk()
+	t.AssertContentType("application/json; charset=utf-8")
 }
 
 func (t *ApplicationTest) TestThatRedisAPIWorks() {
-    t.Get("/redis")
-    t.AssertOk()
-    t.AssertContentType("application/json; charset=utf-8")
+	t.Get("/redis")
+	t.AssertOk()
+	t.AssertContentType("application/json; charset=utf-8")
 }
 
 func (t *ApplicationTest) After() {
-    println("Tear down")
+	println("Tear down")
 }
-
