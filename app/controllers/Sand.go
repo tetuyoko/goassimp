4252b40f53933c557d80b0dea82716da2ab5c693
tetuyoko <tyokoyama53@gmail.com>
2015-06-29 16:27:35 +0900

    rename file

---
+++ app/controllers/Sand.go
@@ -0,0 +1,45 @@
+package controllers
+
+import (
+	"fmt"
+	"github.com/revel/revel"
+	"goassimp/app/models"
+	"goassimp/app/routes"
+
+	"goassimp/app/mgnredis"
+)
+
+type Sand struct {
+	App
+}
+
+func (c *Sand) List() revel.Result {
+	greeting := "Super Mother Fucing Hage!!"
+	unko := "unko"
+	return c.RenderJson(map[string]interface{}{
+		"Count":    4,
+		"unko":     unko,
+		"greeting": greeting,
+		"Status":   "Successfully uploaded",
+	})
+}
+
+func (c *Sand) Show(id int) revel.Result {
+	oem := models.Oembed{Version: 1, Type: "fuga"}
+	return c.RenderJson(oem)
+}
+
+func (c *Sand) Cancel(id int) revel.Result {
+	c.Flash.Success(fmt.Sprintln("Booking cancelled for confirmation number", id))
+	return c.Redirect(routes.Sand.Index())
+}
+
+func (c *Sand) Index() revel.Result {
+	greeting := "Super Mother Fucing Hage!!"
+	info, err := mgnredis.RedisDb.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s", info)
+	return c.Render(greeting, "unko")
+}
