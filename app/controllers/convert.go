a17bf4d573e812a15e5e668c0cb687fb96f54394
tetuyoko <tyokoyama53@gmail.com>
2015-06-29 16:34:51 +0900

    convert controllerの作成

--- app/controllers/convert.go
+++ app/controllers/convert.go
@@ -9,26 +9,26 @@ import (
 	"os"
 )
 
-type Single struct {
+type Convert struct {
 	App
 }
 
-func (c *Single) Upload() revel.Result {
+func (c *Convert) List() revel.Result {
 	var id string = c.Params.Get("id")
 	fmt.Println(id)
 	return c.Render()
 }
 
-func (c *Single) HandleUpload(avatar []byte) revel.Result {
+func (c *Convert) Convert(source[]byte) revel.Result {
 	dstDir := "public/tmp"
-	dstName := c.Params.Files["avatar"][0].Filename
+	dstName := c.Params.Files["source"][0].Filename
 	dst, err := os.Create(dstDir + "/" + dstName)
 	if err != nil {
 		panic(err)
 	}
 	defer dst.Close()
 
-	_, err = io.Copy(dst, bytes.NewReader(avatar))
+	_, err = io.Copy(dst, bytes.NewReader(source))
 	if err != nil {
 		panic(err)
 	}
