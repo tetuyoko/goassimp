d0f88174042e9b11e5609db4d387fc3f2804051d
tetuyoko <tyokoyama53@gmail.com>
2015-06-29 16:38:39 +0900

    commentを追加

--- app/controllers/convert.go
+++ app/controllers/convert.go
@@ -16,10 +16,13 @@ type Convert struct {
 func (c *Convert) List() revel.Result {
 	var id string = c.Params.Get("id")
 	fmt.Println(id)
+	// 情報出力
+	// Zset順
 	return c.Render()
 }
 
 func (c *Convert) Convert(source[]byte) revel.Result {
+	// 画像保存
 	dstDir := "public/tmp"
 	dstName := c.Params.Files["source"][0].Filename
 	dst, err := os.Create(dstDir + "/" + dstName)
@@ -32,6 +35,11 @@ func (c *Convert) Convert(source[]byte) revel.Result {
 	if err != nil {
 		panic(err)
 	}
+
+	// 情報登録
+	// uuid, path, created_at
+	// Zset順
+
 
 	fmt.Println("Status: Successfully uploaded")
 
