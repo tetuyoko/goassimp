0cd91eae3001935cb5696c3d746c996a59b58d7d
tetuyoko <tyokoyama53@gmail.com>
2015-06-30 12:45:35 +0900

    create databases;

--- app/lib/mugendb.go
+++ app/lib/mugendb.go
@@ -4,6 +4,7 @@ import (
     "github.com/jinzhu/gorm"
     _ "github.com/go-sql-driver/mysql"
     "log"
+    "fmt"
 )
 
 var (
@@ -11,9 +12,20 @@ var (
 )
 
 func InitDB() {
+    fmt.Println("called")
     var err error
-    Db, err = gorm.Open("mysql", "root:@/godb?charset=utf8&parseTime=True&loc=Local")
+
+    // connect mysql
+    Db, err = gorm.Open("mysql", "root:@/?charset=utf8&parseTime=True&loc=Local")
     checkErr(err, "mysql Open failed.")
+
+    // create db if not exists
+    Db.Exec( "CREATE DATABASE IF NOT EXISTS godb DEFAULT CHARACTER SET utf8;")
+
+    // connect db
+    Db, err = gorm.Open("mysql", "root:@/godb?charset=utf8&parseTime=True&loc=Local")
+    checkErr(err, "mysql Connect failed.")
+
     Db.DB().Ping()
     Db.DB().SetMaxIdleConns(10)
     Db.DB().SetMaxOpenConns(100)
