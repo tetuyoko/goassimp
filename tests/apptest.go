9173a210dcdd73d0ac48806f10f58869ab2a7623
tetuyoko <tyokoyama53@gmail.com>
2015-07-18 14:05:44 +0900

    add test

--- tests/apptest.go
+++ tests/apptest.go
@@ -1,21 +1,71 @@
 package tests
 
-import "github.com/revel/revel/testing"
+import (
+	"github.com/revel/revel/testing"
+	"net/url"
+)
 
-type AppTest struct {
+type ApplicationTest struct {
 	testing.TestSuite
 }
 
-func (t *AppTest) Before() {
+func (t *ApplicationTest) Before() {
 	println("Set up")
 }
 
-func (t *AppTest) TestThatIndexPageWorks() {
+func (t *ApplicationTest) TestThatIndexPageWorks() {
 	t.Get("/")
 	t.AssertOk()
 	t.AssertContentType("text/html; charset=utf-8")
 }
 
-func (t *AppTest) After() {
+func (t *ApplicationTest) TestThatRedisAPIWorks() {
+	t.Get("/redis")
+	t.AssertOk()
+	t.AssertContains("Success")
+	t.AssertContentType("application/json; charset=utf-8")
+}
+
+func (t *ApplicationTest) TestThatRedisPingAPIWorks() {
+	t.Get("/redis/ping")
+	t.AssertOk()
+	t.AssertContains("Success")
+	t.AssertContentType("application/json; charset=utf-8")
+}
+
+func (t *ApplicationTest) TestThatRedisSetGetAPIWorks() {
+	t.PostForm("/redis/set", url.Values{
+		"key": {"hage"},
+		"val": {"1"},
+	})
+	t.AssertOk()
+	t.AssertContains("OK")
+	t.AssertContentType("application/json; charset=utf-8")
+
+	t.Get("/redis/get/hage")
+
+	t.AssertOk()
+	t.AssertContains("1")
+	t.AssertContentType("application/json; charset=utf-8")
+}
+
+func (t *ApplicationTest) TestThatRedisHSetHGetAPIWorks() {
+	t.PostForm("/redis/hset", url.Values{
+		"key":   {"hsetkey"},
+		"field": {"hsetfield"},
+		"val":   {"1"},
+	})
+	t.AssertOk()
+	t.AssertContentType("application/json; charset=utf-8")
+
+	t.Get("/redis/hget/hsetkey")
+
+	t.AssertOk()
+	t.AssertContains("hsetfield")
+	t.AssertContains("1")
+	t.AssertContentType("application/json; charset=utf-8")
+}
+
+func (t *ApplicationTest) After() {
 	println("Tear down")
 }
